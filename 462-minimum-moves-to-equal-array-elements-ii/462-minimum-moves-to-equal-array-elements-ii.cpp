b120d92e5e4b30c74c0b983d138ba3a9bcedb63a
shubhangi26 <46265000+shubhangi26@users.noreply.github.com>
2022-06-30 12:38:50 +0530

    Time: 21 ms (26.38%), Space: 10.9 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     int minMoves2(vector<int>& nums) {
       int n = nums.size();
-        //sort , find median ,make every element equal to median 
+        //approach 1 :sort , find median ,make every element equal to median 
+        //approach 2 :sort , min+x ,max-y, we need to calculate x+y which are the steps
         sort (nums.begin(),nums.end()); 
         int res=0;
-        for(auto i:nums)
-            res+=abs(i-nums[n/2]);
+        int i=0;
+        int j =n-1;
+        while(i<j){  //at middle there is median there they will be equal
+            res+=abs(nums[j--]-nums[i++]);
+        }
+            
         return res;
         
     }
