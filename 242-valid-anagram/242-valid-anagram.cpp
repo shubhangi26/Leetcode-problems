83b3901f97f977422a6acc3e2a19cc66b84dd53a
shubhangi26 <46265000+shubhangi26@users.noreply.github.com>
2022-07-05 01:53:49 +0530

    Time: 10 ms (73.88%), Space: 7.1 MB (94.63%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      if(s.length()!=t.length()) return false;
+      unordered_map<char,int> mp;
+      for (int i = 0 ;i<s.length();i++){
+          mp[s[i]]++;
+          mp[t[i]]--;
+      }
+        for (auto m : mp){
+            if(m.second) return false;
+        
+        }
+        return true;
+    }
+};
