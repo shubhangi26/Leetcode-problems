891fa851c6a765ea56a23d6865dfe80d90e15ab0
shubhangi26 <46265000+shubhangi26@users.noreply.github.com>
2022-07-29 11:09:14 +0530

    Time: 6 ms (83.06%), Space: 7 MB (46.04%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -3,6 +3,7 @@ public:
     bool isIsomorphic(string s, string t) {
       vector<int> m1(128,-1);
       vector<int> m2(128,-1);
+        if(s.length()!=t.length()) return false;
         for (int i = 0; i < s.size(); i++) {
             if (m1[s[i]] != m2[t[i]]) return false;
             m1[s[i]] = i;
